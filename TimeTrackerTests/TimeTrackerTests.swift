2e7f1d587d22b1948a728b6f1535e9532faab3cc
Rodrigo Castrejon <me@rcastrejon.dev>
2025-03-26 12:42:39 -0600

    Initial Commit

---
+++ TimeTrackerTests/TimeTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerTests.swift
+//  TimeTrackerTests
+//
+//  Created by Rodrigo Castrejón on 26/03/25.
+//
+
+import Testing
+@testable import TimeTracker
+
+struct TimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
